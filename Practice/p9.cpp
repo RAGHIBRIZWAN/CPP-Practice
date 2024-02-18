80baaf417efc6b00b78221e3545de705c325c23e
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-02-18 23:39:28 +0500

    Create p9.cpp

---
+++ Practice/p9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    
+    for(int i = 1; i <= n; i++ ){
+        int temp = i * i;
+        cout<<temp<<endl;
+    }
+
+    return 0;
+}
