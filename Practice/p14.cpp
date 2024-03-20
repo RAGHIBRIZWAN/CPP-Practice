38bff6904adcb6e540d9c0873b0d7e614381d45d
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-03-20 22:46:45 +0500

    Update p14.cpp

--- Practice/p14.cpp
+++ Practice/p14.cpp
@@ -1,66 +1,93 @@
 #include<iostream>
+#include<string>
+
 using namespace std;
 
 class User;
 
-class Comment{
-protected:
+class Comment {
+public:
     int commentId;
     string content;
-    User* author;
+    const User* author; 
 public:
-    Comment(){}
-    Comment(const int &commentId,const string &content, const User* author):commentId(commentId),content(content),author(author){}
+    Comment() {}
+    Comment(const int &commentId, const string &content, const User* author) : commentId(commentId), content(content), author(author) {}
 };
 
-class Post{
-protected:
+
+class Post {
+public:
+    static const int count = 5;
     int postID;
     string content;
-    Comment comments[3];
+    Comment comments[count];
+    int commentCount;
 
 public:
-    Post(){}
-    Post(const int &postId, const string &content,const Comment &comm){
-        this-> postID = postID;
-        this-> content = content;
-        for (int i = 0; i < 3; i++)
-        {
-            comments[i] = comm[i];
-        }   
-    }
-};
+    Post() : commentCount(0) {}
+    Post(const int &postId, const string &content) : postID(postId), content(content), commentCount(0) {}
 
-class User{
-protected:
-    int userId;
-    string userName;
-    Post posts[3];
-
-public:
-    User(){}
-    User(const int &userId, const string &userName,const Post &post){
-        this-> userId = userId;
-        this-> userName = userName;
-        for (int i = 0; i < 3; i++)
-        {
-            posts[i] = post[i];
-        }   
-    }
-};
-
-class SocialMedia{
-protected:
-    User users[3];
-public:
-    User(const User &user){
-        for (int i = 0; i < 3; i++)
-        {
-            users[i] = user[i];
+    void AddNew(const Comment& value) {
+        if (commentCount < count) {
+            comments[commentCount++] = value;
         }
     }
 };
 
-int main(){
+class User {
+public:
+    static const int count = 5;
+    int userId;
+    string userName;
+    Post posts[count];
+    int postCount;
 
+public:
+    User() : postCount(0) {}
+    User(const int &userId, const string &userName) : userId(userId), userName(userName), postCount(0) {}
+
+    void CreateNew(const Post& value) {
+        if (postCount < count) {
+            posts[postCount++] = value;
+        }
+    }
+
+    void display() {
+        for (int i = 0; i < postCount; ++i) {
+            cout << posts[i].content << endl;
+        }
+    }
+};
+
+class SocialMedia {
+public:
+    static const int count = 5;
+    User users[count];
+    int userCount;
+
+public:
+    SocialMedia() : userCount(0) {}
+
+    void RegisterNew(const User& value) {
+        if (userCount < count) {
+            users[userCount++] = value;
+        }
+    }
+};
+
+int main() {
+    User user(1, "Alice");
+    Post post(1, "Hello, World!");
+    Comment comment(1, "Nice post!", &user);
+
+    user.CreateNew(post);
+    post.AddNew(comment);
+
+    SocialMedia socialMedia;
+    socialMedia.RegisterNew(user);
+
+    user.display();
+
+    return 0;
 }
