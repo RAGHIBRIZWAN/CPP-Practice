4bda192d3bacd06d8cf44d301193cefff4afb74f
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-04-09 12:47:38 +0500

    Update p19.cpp

--- Practice/p19.cpp
+++ Practice/p19.cpp
@@ -27,3 +27,43 @@
         }
     }    
     cout<<"Greatest Array is: "<<value;
+
+    // Maximum number of 1's in a row using vector
+
+    vector<vector<int>> v(3, vector<int>(4, 0));
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin>>v[i][j];
+        }
+        
+    }
+    
+    int count,value=0,index=0;
+    vector<int> w;
+    
+    for (int i = 0; i < 3; i++)
+    {
+        count=0;
+        for (int j = 0; j < 4; j++)
+        {
+            if (v[i][j] == 1)
+            {
+                count++;
+            }
+        }
+        w.push_back(count);
+    }
+
+    value = w[0];
+
+    for (int i = 1; i < 3; i++)
+    {
+        if (w[i] > value)
+        {
+            value = w[i];
+        }
+    }    
+    cout<<"Greatest Array is: "<<value;
