63e8ffd9f33518f9c0b7311633ef35d54526937e
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-04-11 13:49:37 +0500

    Create LeetCode_3Sum.cpp

---
+++ Practice/LeetCode_3Sum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    // USING ARRAY
+
+    int arr[] = {-1,0,1,2,-1,-4};
+
+    for (int i = 0; i < sizeof(arr)/sizeof(arr[0])/2; i++)
+    {
+        for (int j = i+1; j < sizeof(arr)/sizeof(arr[0])/2; j++)
+        {
+            for (int k = j+1; k < sizeof(arr)/sizeof(arr[0]); k++)
+            {
+                if (arr[i]+arr[j]+arr[k] == 0 && i!= j && j!=k && i!=k)
+                {
+                    cout<<"["<<arr[i]<<","<<arr[j]<<","<<arr[k]<<"]"<<endl;
+                }   
+            }   
+        }   
+    }  
+}
