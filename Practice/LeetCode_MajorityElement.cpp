06e21d5e0fd56158e9fc03372a6ef3f1ba0b9cd6
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-04-13 00:38:36 +0500

    Create LeetCode_MajorityElement.cpp

---
+++ Practice/LeetCode_MajorityElement.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    vector<int> nums = {1,2,2};
+    int count = 0;
+    int maxCount = 0;
+    int majorityElement = 0;
+    
+    sort(nums.begin(), nums.end());
+    if (nums.size() == 1)
+    {
+        cout<< nums[nums.size()-1];
+        return 0;
+    }
+    
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == nums[i+1])
+        {
+            count++;
+            if (count > maxCount) {
+                maxCount = count;
+                majorityElement = nums[i];
+            }
+        }
+        else {
+            count = 0;
+        }
+    }
+
+    cout << majorityElement << endl;
+}
