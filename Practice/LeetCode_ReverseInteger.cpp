d99eeb9b0c321dfce02b7663a0e145d88501d4a0
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-04-17 00:39:10 +0500

    Update LeetCode_ReverseInteger.cpp

--- Practice/LeetCode_ReverseInteger.cpp
+++ Practice/LeetCode_ReverseInteger.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
+    // METHOD 1:
     int num = 1534236469;
     string b = to_string(num);
     int i = 0;
@@ -25,4 +26,17 @@ int main(){
   
     int c = stoi(b);  
     cout<<c;
+
+
+    //METHOD 2:
+     int reverse(int x) {
+int reversed = 0;
+while (x != 0) {
+        int digit = x % 10;
+        x /= 10;
+        if (reversed > INT_MAX / 10 || reversed < INT_MIN / 10) return 0;
+        reversed = reversed * 10 + digit;
+    }
+    return reversed;
+    }
 }
