8fa83bd7aef99a64541ab61cdb812643d24a7683
RaghibRizwan <raghibrizwan007@gmail.com>
2024-06-12 18:30:26 +0500

    Add new program

---
+++ BuddyStrings.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    string s = "ab",goal = "ba";
+    
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] != goal[i])
+        {
+            
+        }
+    }
+}
