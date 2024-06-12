7db1f9e64f6de7f9f5b959b8a7ea291059597c37
RaghibRizwan <raghibrizwan007@gmail.com>
2024-06-12 19:00:07 +0500

    Add new program

---
+++ LeetCode_AddToArrayFormOfInt.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    vector<int> nums= {9,9,9,9,9,9,9,9,9,9};
+    // vector<int> nums;
+    int k = 1;
+    string num1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        num1 += to_string(nums[i]);
+    }
+
+    int num2 = stoi(num1);
+
+    num2 += k;
+
+    string num3 = to_string(num2);
+
+    nums.clear();
+
+    for (int i = 0; i < num3.length(); i++)
+    {
+        nums.push_back(num3[i] - '0');
+    }
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout<<nums[i]<<" ";
+    }
+    
+     
+}
